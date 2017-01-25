9a82ce4ff3a8c5322ff7fc88b5e285321d27dd35
alex rodriguez <alex.rodriguez@austintexas.gov>
2017-01-25 15:14:12 -0600

    add user controller. provide user profile to child components

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,12 @@ var express = require('express');
 var router = express.Router();
 var User = require('../model/model');
 
+router.get('/api/user/test', function (req, res, next) {
+  res.json({you: 'are an idiot'})
+})
+
+module.exports = router;
+
 // router.get('/api/user/check', function(req, res, next) {
   
 //   res.send({user: 'user'});
--- src/components/JobLister/JobLister.js
+++ src/components/JobLister/JobLister.js
@@ -20,6 +20,9 @@ class JobLister extends Component {
   }
 }
 
+JobLister.propTypes = {
+  job: React.PropTypes.object.isRequired
+}
 
 
 
