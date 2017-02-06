3b00e6f2ed34545ff30accf88ac048a7a220268e
alex rodriguez <alex.rodriguez@austintexas.gov>
2017-02-06 09:51:22 -0600

    login to dashboard. add application state to dashboard component

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -5,13 +5,14 @@ export default function authReducer(state = {
   isAuthenticated: !AuthService.isTokenExpired(),
   isFetching: false,
   profile: AuthService.getProfile(),
+  savedJobs: [],
   error: null
 }, action) {
   switch (action.type) {
     case ActionTypes.LOGIN_REQUEST:
       return {...state, isFetching: true, error: null}
     case ActionTypes.LOGIN_SUCCESS:
-      return {...state, isFetching: false, isAuthenticated: true, profile: action.profile}
+      return {...state, isFetching: false, isAuthenticated: true, profile: action.profile, savedJobs: action.savedJobs}
     case ActionTypes.LOGIN_ERROR:
       return {...state, isFetching: false, isAuthenticated: false, profile: {}, error: action.error}
     case ActionTypes.LOGOUT_SUCCESS:
